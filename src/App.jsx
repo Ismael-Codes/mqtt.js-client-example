3f60527c77012fc55d9b7ddb51e3cdc1549d1cf2
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-09-09 15:12:26 -0500

    react-paho-mqtt added

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,87 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import mqtt from "precompiled-mqtt";
-// import mqtt from 'mqtt'
+// import mqtt from "precompiled-mqtt";
+import React from 'react';
+
+import * as mqtt from 'react-paho-mqtt';
 
 function App() {
 
-  const options = {
-    username: 'ismael',
-    password: 'abejareina',
-    rejectUnauthorized: false
-  };
+  const [client, setClient] = React.useState(null);
+  const _topic = ["brianelchido"]; //Topic al que quiere conectarse
+  const _options = {}; //Options
 
-  const URL = "mqtt://test.mosquitto.org:8081";
+  React.useEffect(() => {
+    _init();
+  }, [])
 
-  // const URL = "tls:173eaa144cef42b4aeacd8eeb6470ce0.s1.eu.hivemq.cloud:8883";
-
-  // connect to your cluster, insert your host name and port
-  // const client = mqtt.connect('wss:173eaa144cef42b4aeacd8eeb6470ce0.s1.eu.hivemq.cloud:8883', options);
-
-  // const client = mqtt.connect({hostname: 'mqtt://test.mosquitto.org', port: 8080});
-  const client = mqtt.connect(URL, options);
-
-  // console.log(client)
-
-  // prints a received message
-  client.on('message', function (topic, message) {
-    console.log(String.fromCharCode.apply(null, message)); // need to convert the byte array to string
-  });
-
-  // reassurance that the connection worked
-  client.on('connect', () => {
-    console.log('Connected!');
-  });
-
-  // prints an error message
-  client.on('error', (error) => {
-    console.log('Error:', error);
-  });
-
-  const user = {
-    name: 'Jack',
-    isMarried: false,
-    age: 25,
+  const _init = () => {
+    const c = mqtt.connect("broker.mqttdashboard.com", Number(8000), "clientId-", _onConnectionLost, _onMessageArrived); // mqtt.connect(host, port, clientId, _onConnectionLost, _onMessageArrived)
+    setClient(c);
   }
 
-  const userJSON = JSON.stringify(user);
+  // called when sending payload
+  const _sendPayload = () => {
+    const payload = mqtt.parsePayload("Hello", "World"); // topic, payload
+    client.send(payload);
+  }
 
-  client.subscribe('testtopic/2');
-  // client.publish('testtopic/2', userJSON);
+  // called when client lost connection
+  const _onConnectionLost = responseObject => {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost: " + responseObject.errorMessage);
+    }
+  }
 
+  // called when messages arrived
+  const _onMessageArrived = message => {
+    console.log("onMessageArrived: " + message.payloadString);
+  }
+
+
+  // called when subscribing topic(s)
+  const _onSubscribe = () => {
+    client.connect({
+      onSuccess: () => {
+        for (var i = 0; i < _topic.length; i++) {
+          client.subscribe(_topic[i], _options);
+        }
+      }
+    }); // called when the client connects
+  }
+
+  // called when subscribing topic(s)
+  const _onUnsubscribe = () => {
+    for (var i = 0; i < _topic.length; i++) {
+      client.unsubscribe(_topic[i], _options);
+    }
+  }
+
+  // called when disconnecting the client
+  const _onDisconnect = () => {
+    client.disconnect();
+  }
 
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className="App-header">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button
+          style={{ color: 'white' }}
+          onClick={_onSubscribe}>
+          <h1>Subscribe Topic</h1>
+        </button>
+        <button
+          style={{ color: 'white' }}
+          onClick={_sendPayload}>
+          <h1>Send Message</h1>
+        </button>
+      </header>
     </div>
   )
 }
